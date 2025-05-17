084bf359724d067db498542af483aca162cbe101
EnGenius <atiqqq0@gmail.com>
2025-05-17 15:30:31 +0100

    update metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,30 @@ const audiowide = Audiowide({
 export const metadata = {
   title: "QR Code Generator",
   description: "Generate QR codes easily",
+  openGraph: {
+    title: "QR Code Generator",
+    description: "Generate QR codes easily",
+    url: "https://qr-code-by-atiq.vercel.app/",
+    siteName: "https://qr-code-by-atiq.vercel.app/",
+    images: [
+      {
+        url: "https://qr-code-by-atiq.vercel.app/preview.png", 
+        width: 1200,
+        height: 630,
+        alt: "QR Code Generator",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "QR Code Generator",
+    description: "Generate QR codes easily",
+    images: ["https://qr-code-by-atiq.vercel.app/preview.png"],
+    creator: "@maybeatiq",
+  },
+
   icons: {
     icon: "/logo.png",
   },
